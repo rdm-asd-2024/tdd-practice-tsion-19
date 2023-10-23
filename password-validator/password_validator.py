63582dbe2e5c0a46e7b7a3e53e28c639171d22eb
tsion-19 <tsiongher2013@gmail.com>
2023-10-23 03:18:10 +0300

    my first  commit

--- password-validator/password_validator.py
+++ password-validator/password_validator.py
@@ -1,31 +1,59 @@
-# password_validator.py
+import re
+import json
+import csv
 
 class PasswordValidator:
-    def __init__(self, min_length=8, max_length=20, allowed_chars=None, forbidden_words=None):
-        self.min_length = min_length
-        self.max_length = max_length
-        self.allowed_chars = allowed_chars or set()
-        self.forbidden_words = forbidden_words or []
-
+    def __init__(self, custom_validators=None):
+        self.custom_validators = custom_validators if custom_validators else []
+    
     def is_valid(self, password):
-        # Check minimum and maximum length
-        if len(password) < self.min_length or len(password) > self.max_length:
-            return False
+        # Check length
+        if not 8 <= len(password) <= 16:
+            return False, "Password length must be between 8 and 16 characters."
+        
+        # Check for uppercase letter and digit
+        if not any(char.isupper() for char in password) or not any(char.isdigit() for char in password):
+            return False, "Password must have at least one uppercase letter and one digit."
+        
+        # Check for at least 4 distinct characters
+        if len(set(password)) < 4:
+            return False, "Password must have at least 4 distinct characters."
+        
+        # Check for adjacent characters
+        if any(password[i] == password[i+1] for i in range(len(password) - 1)):
+            return False, "Password can't contain adjacent characters."
+        
+        # Apply custom validators
+        for validator in self.custom_validators:
+            if not validator(password):
+                return False, "Custom validation failed."
 
-        # Check allowed characters
-        if any(char not in self.allowed_chars for char in password):
-            return False
+        return True, "Password is valid."
 
-        # Check for forbidden words
-        if any(word in password for word in self.forbidden_words):
-            return False
+def custom_validator_example(password):
+    # Add your custom validation logic here
+    # Return True if the password is valid, False otherwise
+    return True
 
-        return True
+def main():
+    # Create a PasswordValidator instance with custom validators
+    validator = PasswordValidator(custom_validators=[custom_validator_example])
 
-def compose_criteria(criteria_list):
-    def validate(password):
-        for criteria in criteria_list:
-            if not criteria.is_valid(password):
-                return False
-        return True
-    return validate
+    # Example: Using JSON input
+    json_data = [
+        {"password": "Pass1234"},
+        {"password": "Invalid"},
+    ]
+    for data in json_data:
+        is_valid, message = validator.is_valid(data["password"])
+        print(f"Password: {data['password']} - Valid: {is_valid} - Reason: {message}")
+
+    # Example: Using CSV input
+    csv_file = "passwords.csv"
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            is_valid, message = validator.is_valid(row["password"])
+            print(f"Password: {row['password']} - Valid: {is_valid} - Reason: {message}")
+if __name__ == "__main__":
+    main()
--- rover/MoonRover.py
+++ rover/MoonRover.py
@@ -1,57 +1,110 @@
-import argparse
-
 class MoonRover:
-    def __init__(self, grid_size):
-        self.grid = [[0] * grid_size for _ in range(grid_size)]
-        self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # Right, Left, Down, Up
-        self.x, self.y = 0, 0  # Initial position
-        self.route = []  # To store the route
+    def __init__(self, obstacles=[]):
+        self.x = 0
+        self.y = 0
+        self.direction = 'N'  # Initial direction: North
+        self.obstacles = set(obstacles)
+        self.route = []
 
     def move(self, commands):
         for command in commands:
             if command == 'R':
-                #self._move_to(0)  # Move right
-                print("yeah")
+                self.rotate_right()
             elif command == 'L':
-                self._move_to(1)  # Move left
+                self.rotate_left()
             elif command == 'U':
-                self._move_to(3)  # Move up
+                self.move_up()
             elif command == 'D':
-                self._move_to(2)  # Move down
+                self.move_down()
+            elif command == 'F':
+                self.move_forward()
+            elif command == 'W':
+                self.move_backward()
             else:
-                raise ValueError("Invalid command")
-
-    def _move_to(self, new_direction):
-        new_x, new_y = self.x + self.directions[new_direction][0], self.y + self.directions[new_direction][1]
-        if self._is_valid_move(new_x, new_y):
-            self.x, self.y = new_x, new_y
-            self.route.append((self.x, self.y))
-
-    def _is_valid_move(self, new_x, new_y):
-        if 0 <= new_x < len(self.grid) and 0 <= new_y < len(self.grid[0]) and self.grid[new_x][new_y] != 1:
-            return True
-        else:
+                raise ValueError(f"Invalid command: {command}")
+    
+    def rotate_right(self):
+        if self.direction == 'N':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'W':
+            self.direction = 'N'
+    
+    def rotate_left(self):
+        if self.direction == 'N':
+            self.direction = 'W'
+        elif self.direction == 'W':
+            self.direction = 'S'
+        elif self.direction == 'S':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'N'
+    
+    def move_up(self):
+        new_y = self.y + 1
+        if self.check_obstacle(self.x, new_y):
             raise Exception("Obstacle detected")
+        self.y = new_y
+        self.route.append((self.x, new_y))
+    
+    def move_down(self):
+        new_y = self.y - 1
+        if self.check_obstacle(self.x, new_y):
+            raise Exception("Obstacle detected")
+        self.y = new_y
+        self.route.append((self.x, new_y))
+    
+    def move_forward(self):
+        new_x, new_y = self.calculate_forward_position()
+        if self.check_obstacle(new_x, new_y):
+            raise Exception("Obstacle detected")
+        self.x = new_x
+        self.y = new_y
+        self.route.append((new_x, new_y))
+    
+    def move_backward(self):
+        new_x, new_y = self.calculate_backward_position()
+        if self.check_obstacle(new_x, new_y):
+            raise Exception("Obstacle detected")
+        self.x = new_x
+        self.y = new_y
+        self.route.append((new_x, new_y))
 
-    def draw_route(self):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[i])):
-                if (i, j) == (self.x, self.y):
-                    print('R', end=' ')
-                elif (i, j) in self.route:
-                    print('.', end=' ')
-                else:
-                    print(' ', end=' ')
-            print()
+    def calculate_forward_position(self):
+        if self.direction == 'N':
+            return self.x, self.y + 1
+        elif self.direction == 'E':
+            return self.x + 1, self.y
+        elif self.direction == 'S':
+            return self.x, self.y - 1
+        elif self.direction == 'W':
+            return self.x - 1, self.y
 
-def main():
-    parser = argparse.ArgumentParser(description="Moon Rover Simulation")
-    parser.add_argument("commands", type=str, help="String of commands (R, L, U, D) for the rover")
-    args = parser.parse_args()
+    def calculate_backward_position(self):
+        if self.direction == 'N':
+            return self.x, self.y - 1
+        elif self.direction == 'E':
+            return self.x - 1, self.y
+        elif self.direction == 'S':
+            return self.x, self.y + 1
+        elif self.direction == 'W':
+            return self.x + 1, self.y
+    
+    def check_obstacle(self, x, y):
+        return (x, y) in self.obstacles
 
-    rover = MoonRover(10)  # Assuming a 10x10 grid
-    rover.move(args.commands)
-    rover.draw_route()
+    def print_route(self):
+        for x, y in self.route:
+            print(f"({x}, {y})")
+obstacles = [(1, 1), (2, 2)]
+rover = MoonRover(obstacles)
 
-if __name__ == "__main__":
-    main()
+try:
+    rover.move("FRFFL")  # Example commands
+except Exception as e:
+    print(e)
+
+rover.print_route()  # Print the route the rover has followed
